bc6084ff61d4bfb0ec52ecff2f7007346ad1d16c
rockyjain <jainrocky1008@gmail.com>
2019-10-29 02:42:57 +0530

    added obst

---
+++ ALGO/dynamic_programming/README.md
@@ -0,0 +1,33 @@
+# Dynamic Programming
+
+## 0_1 knapsack
+
+Given weights and values of `n` items, put these items in a knapsack of capacity `W` to get the maximum total value in the knapsack.
+
+- Wosrt/Avg/Best -case: `O(n*W)`
+
+## Longest common subsequence
+
+Given two sequences(let say x and y), find the length of longest subsequence present in both of them. A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous. 
+
+`n <- len(x)`
+`m <- len(y)`
+
+- Wosrt/Avg/Best -case: `O(n*m)`
+
+## Matrix chain multiplication
+
+- Given a sequence of matrices, find the most efficient way to multiply these matrices together.
+- The problem is not actually to perform the multiplications, but merely to decide in which order to perform the multiplications.
+
+`n <- Number of matrices`
+- Wosrt/Avg/Best -case: `O(n^3)`
+
+## Optimal binary search tree
+
+- Given a sorted array keys[0.. n-1] of search keys and an array freq[0.. n-1] of frequency counts, where freq[i] is the number of searches to keys[i]
+- Construct a binary search tree of all keys such that the total cost of all the searches is as small as possible.
+
+`n <- Number of keys`
+- Wosrt/Avg/Best -case: `O(n^3)`
+
--- ALGO/dynamic_programming/dp__optimal_binary_search_tree.py
+++ ALGO/dynamic_programming/dp__optimal_binary_search_tree.py
@@ -1,9 +1,58 @@
 import os, sys, warnings
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'DS', 'tree' ))
+from bst import BSTree
 
+inf = 2**31
 
-def obst_cost():
-    pass
+def obst_cost(k, f, visualise=None):
+    n = len(k)
+    cache = [[0]*(n+1) for i in range(n+1)]
+    p_cache = [[0]*(n+1) for i in range(n+1)]
 
+    w = [0]
+    for i in range(n):
+        w.append(w[i]+f[i])
+        
+    for i in range(n):    
+        cache[i][i] = 0
+
+    for d in range(1, n+1):
+        for start in range(0, n-d+1):
+            end = start+d
+            cache[start][end]=inf
+            for r in range(start, end):
+                temp = cache[start][r] + cache[r+1][end] + w[end] - w[start]
+                if temp < cache[start][end]:
+                    cache[start][end] = temp
+                    p_cache[start][end-1] = r
+                    
+    if visualise:
+        tree = BSTree()
+        def print_obst(l, r):
+            if l <= r:
+                print(k[p_cache[l][r]], f[p_cache[l][r]])
+                tree.add_node(BSTree.Node( k[p_cache[l][r]] ))
+                print_obst(l, p_cache[l][r]-1)
+                print_obst(p_cache[l][r]+1, r)
+        print_obst(0, n-1)
+        tree.draw()
+    
+    return cache[0][n]
+            
+    
+'''
+Note: First graph window is of BST(before optimisation) close it to see after optimisation BST(or OBST)
+'''
 
 if __name__=='__main__':
-    pass
+    k = [10, 20, 30, 40, 50, 60, 70, 80, 90, 99]
+    f = [71, 43, 321, 214, 142, 123, 124, 192, 431, 224]
+    tree = BSTree()
+    for n in k:
+        tree.add_node(BSTree.Node( n ))
+    tree.draw()
+    print('Max Cost:', obst_cost(k, f, visualise=True))
+
+
+
+
